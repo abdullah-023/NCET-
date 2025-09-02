d47f6d8353615969745c963aedd9e7caba31aa1b
abdul <abdul@LAPTOP-LE5RN606>
2025-09-02 21:37:16 +0530

    3rd commit - CRUD in Spring Boot using list(temp)

---
+++ CRUD-spring-web/src/test/java/ncet/day12/FirstSpringStarterProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ncet.day12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringStarterProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
